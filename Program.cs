554d0e4fdb654092cdff8fc2ba404b670370936d
Matheus Vaz <matheusvaz1998@hotmail.com>
2023-09-01 23:29:53 -0300

    Lendo itens dobanco de dados

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using Todo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(); //Adicionando suporte aos controllers
+builder.Services.AddDbContext<AppDbContext>(); //Adicionando suporte para deixar o AppDbContext como um serviço
 
 var app = builder.Build();
 
